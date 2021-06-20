37f5b558572b1aa906eb60ec16b61a93fa5be318
ferbin <ferbin17@gmail.com>
2021-06-20 11:20:46 +0530

    feat : dummy sign out option

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   devise_for :users, controllers: { sessions: 'users/sessions' }
-  resources :users do
+  resources :users, only: [] do
     collection do
       get 'dashboard'
     end
