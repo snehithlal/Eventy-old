96a37a56da42d7e505a91eeb94c8f246b473127c
ferbin <ferbin17@gmail.com>
2021-06-20 11:16:04 +0530

    feat: add devise MVCs

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  resources :users do
+    collection do
+      get 'dashboard'
+    end
+  end
+  
+  root to: "users#dashboard"
 end
