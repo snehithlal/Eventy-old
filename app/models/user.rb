5f735819d82a4fad9ee07ce38851c80dd002ed4f
anbk <ahammednafihbk@gmail.com>
2021-07-09 23:34:20 +0530

    :sparkles: active storage for file upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_secure_password
-
   validates :user_name, presence: true, uniqueness: true, length: { minimum: 3 }
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true
+
+  has_secure_password
+  has_one_attached :avatar
 
   def full_name
     "#{first_name} #{middle_name} #{last_name}".squish
